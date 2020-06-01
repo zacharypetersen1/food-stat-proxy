cd39393fb6ada9f672ef7d52031401de34c8ba16
Zachary Petersen <zacharypetersen1@gmail.com>
2020-06-01 14:46:44 -0700

    Import client credentials from secret file

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
+var request = require("request");
+var { CLIENT_ID, CLIENT_SECRET } = require("./clientCredentials");
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
